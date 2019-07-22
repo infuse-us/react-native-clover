90ed140906b2895c8bb0bf7ef943bbd663ce47ae
Xing Tong <xing@infuse.us>
2019-07-22 11:30:45 -0700

    Fix readme

--- index.js
+++ index.js
@@ -2,24 +2,8 @@ import { NativeModules } from 'react-native';
 
 const { RNCloverBridge } = NativeModules;
 
-// /**
-//  *
-//  * @param {object} option
-//  * @param {number} option.amount
-//  * @param {string} option.externalPaymentId
-//  * @param {number} [option.cardEntryMethods]
-//  * @param {boolean} [option.disablePrinting=false]
-//  * @param {number} [option.disableReceiptSelection=1]
-//  * @return {promise}
-//  */
-// function sale(option) {
-//   return RNCloverBridge.sale(...option);
-// }
-
 export default {
   ...RNCloverBridge,
-  // sale: RNCloverBridge.sale,
-  // refund: RNCloverBridge.refund,
   isFlex: () => RNCloverBridge.isFlex,
   isMini: () => RNCloverBridge.isMini,
   getSpaVersion: () => RNCloverBridge.getSpaVersion,
